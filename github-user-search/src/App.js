2df4aa7f43a6ecfe3dd88f50f9b8bc88ad1deb28
momarinho <117489486+momarinho@users.noreply.github.com>
2023-01-17 12:23:46 -0300

    functional part

---
+++ github-user-search/src/App.js
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import './App.css';
+
+const API_URL = 'https://api.github.com';
+
+async function fetchResults(query) {
+  try {
+    const response = await fetch(`${API_URL}/search/users?q=${query}`);
+    const json = await response.json();
+    return json.items || [];
+  } catch (e) {
+    throw new Error(e);
+  }
+}
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+  function onSearchChange(event) {
+    setQuery(event.target.value);
+  }
+
+  async function onSearchSubmit(event) {
+    event.preventDefault();
+    const results = await fetchResults(query);
+    setResults(results);
+  }
+
+  return (
+    <div className="app">
+      <main className="main">
+        <Form
+          onChange={onSearchChange}
+          onSubmit={onSearchSubmit}
+          value={query}
+        />
+        <h3>Results</h3>
+        <div id="results">
+          <div>
+            {results.map((user) => (
+              <User
+                key={user.login}
+                avatar={user.avatar_url}
+                url={user.html_url}
+                username={user.login}
+              />
+            ))}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+const User = (props) => {
+  const { avatar, url, username } = props;
+
+  return (
+    <div className="user">
+      <img src={avatar} alt="Profile" width="50" height="50" />
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        {username}
+      </a>
+    </div>
+  );
+};
+
+const Form = (props) => {
+  const { onSubmit, onChange, value } = props;
+
+  return (
+    <form className="search-form" onSubmit={onSubmit}>
+      <input
+        id="search"
+        type="text"
+        placeholder="Enter user or email"
+        onChange={onChange}
+        value={value}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default App;
